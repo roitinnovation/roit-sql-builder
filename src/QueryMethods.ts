dc3168932b625becf212f76158fc920e674f474f
Tiago Gois <tiago.gois@roit.com.br>
2021-07-02 11:34:01 -0300

    fix(logs): remove log properties

--- src/QueryMethods.ts
+++ src/QueryMethods.ts
@@ -3,7 +3,6 @@ import { SqlManager } from "./SQLFileImportService"
 export const buildQueryString = <T>(fileName: string, object: T, hasEmail: boolean = false): string => {
     let query = SqlManager.read(fileName)
     const properties = Object.entries(object)
-    console.log(properties)
     properties.forEach(prop => {
         let [key, value] = prop
         if (typeof value == 'string') {
